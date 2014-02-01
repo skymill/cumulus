7d6950d9fe97bbb4077185d9db76219b7027245b
Sebastian Dahlgren <sebastian.dahlgren@skymill.se>
2014-02-01 13:34:17 +0100

    Updated project header

--- docs/conf.py
+++ docs/conf.py
@@ -50,8 +50,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Cumulus'
-copyright = u'2013, Skymill Solutions'
+project = u'Cumulus Deployment Suite'
+copyright = u'2013-2014, Skymill Solutions'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
