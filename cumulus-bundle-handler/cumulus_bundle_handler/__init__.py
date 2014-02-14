1ba34b953c3bfa3aabd08c916097be04a705b6f4
Sebastian Dahlgren <sebastian.dahlgren@skymill.se>
2014-02-14 10:32:24 +0100

    Splitted out the script execution to it's own module

--- cumulus-bundle-handler/cumulus_bundle_handler/__init__.py
+++ cumulus-bundle-handler/cumulus_bundle_handler/__init__.py
@@ -1,14 +1,13 @@
 """ Script downloading and unpacking Cumulus bundles for the host """
-
 import logging
 import logging.config
 import os
-import subprocess
 import sys
 import tempfile
 import zipfile
 from ConfigParser import SafeConfigParser, NoOptionError
 
+from cumulus_bundle_handler import script_executor
 from cumulus_bundle_handler.command_line_options import ARGS as args
 
 try:
@@ -95,7 +94,7 @@ LOGGER = logging.getLogger('cumulus_bundle_handler')
 
 def main():
     """ Main function """
-    _run_init_scripts(kill=True, start=False, other=True)
+    script_executor.run_init_scripts(kill=True, start=False, other=True)
 
     bundle_types = CONFIG.get('metadata', 'bundle-types').split(',')
     if not bundle_types:
@@ -110,7 +109,7 @@ def main():
     for bundle_type in bundle_types:
         _download_and_unpack_bundle(bundle_type.strip())
 
-    _run_init_scripts(kill=False, start=True, other=True)
+    script_executor.run_init_scripts(kill=False, start=True, other=True)
 
     LOGGER.info("Done updating host")
 
@@ -286,75 +285,6 @@ def _remove_old_files():
 
     # Remove the cache file when done
     os.remove(cache_file)
-
-
-def _run_command(command):
-    """ Run arbitary command
-
-    :type command: str
-    :param command: Command to execute
-    """
-    LOGGER.info('Executing command: {}'.format(command))
-
-    cmd = subprocess.Popen(
-        command,
-        shell=True,
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE)
-
-    stdout, stderr = cmd.communicate()
-    if stdout:
-        print(stdout)
-    if stderr:
-        print(stderr)
-
-    if cmd.returncode != 0:
-        LOGGER.error('Command "{}" returned non-zero exit code {}'.format(
-            command,
-            cmd.returncode))
-        sys.exit(cmd.returncode)
-
-
-def _run_init_scripts(start=False, kill=False, other=False):
-    """ Execute scripts in /etc/cumulus-init.d or C:\\cumulus\\init.d
-
-    :type start: bool
-    :param start: Run scripts starting with S
-    :type kill: bool
-    :param kill: Run scripts starting with K
-    :type others: bool
-    :param others: Run scripts not starting with S or K
-    """
-    init_dir = '/etc/cumulus-init.d'
-    if sys.platform in ['win32', 'cygwin']:
-        init_dir = 'C:\\cumulus\\init.d'
-
-    # Run the post install scripts provided by the bundle
-    if not os.path.exists(init_dir):
-        LOGGER.info('No init scripts found in {}'.format(init_dir))
-        return
-
-    LOGGER.info('Running init scripts from {}'.format(init_dir))
-
-    filenames = []
-    for filename in os.listdir(init_dir):
-        if os.path.isfile(os.path.join(init_dir, filename)):
-            filenames.append(os.path.join(init_dir, filename))
-
-    if start:
-        for filename in filenames:
-            if os.path.basename(filename)[0] == 'S':
-                _run_command(os.path.abspath(filename))
-
-    if kill:
-        for filename in filenames:
-            if os.path.basename(filename)[0] == 'K':
-                _run_command(os.path.abspath(filename))
-
-    if other:
-        for filename in filenames:
-            if os.path.basename(filename)[0] not in ['K', 'S']:
-                _run_command(os.path.abspath(filename))
 
 
 def _store_bundle_files(filenames, extraction_path):
